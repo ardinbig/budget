da5ee3b92fcc20b6021a9b11c31102e313c42141
Ardin Biganiro <ardinbig@gmail.com>
2021-06-23 01:59:54 +0200

    Create project.

---
+++ android/app/src/main/kotlin/com/ardinbig/budget/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ardinbig.budget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Budget',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: Scaffold(),
+    );
+  }
+}
