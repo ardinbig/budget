d17e451926c45702746fb0a4d3ca81fba4e7cc18
Ardin Biganiro <ardinbig@gmail.com>
2021-06-25 00:11:34 +0200

    Add List Category Card

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '/src/data/data.dart';
+import '/src/models/category_model.dart';
 import '/src/widgets/bar_chart.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -11,6 +12,50 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  Widget _buildCategory(Category category, double totalAmountSpent) {
+    return Container(
+      height: 90.0,
+      width: double.infinity,
+      margin: const EdgeInsets.symmetric(horizontal: 18.0, vertical: 8.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            offset: Offset(0, 2),
+            blurRadius: 6.0,
+          ),
+        ],
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                category.name,
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              Text(
+                "\$${(category.maxAmount - totalAmountSpent).toStringAsFixed(2)}"
+                " / \$${category.maxAmount.toStringAsFixed(2)}",
+                style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,23 +87,34 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
             SliverList(
               delegate: SliverChildBuilderDelegate(
-                (context, i) {
-                  return Container(
-                    margin: const EdgeInsets.all(10.0),
-                    decoration: BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black12,
-                          offset: Offset(0, 2),
-                          blurRadius: 6.0,
-                        ),
-                      ],
-                      borderRadius: BorderRadius.circular(10.0),                    
-                    ),
-                    child: BarChart(weeklySpending),
-                  );
+                (_, i) {
+                  if (i == 0) {
+                    return Container(
+                      margin: const EdgeInsets.fromLTRB(15.0, 15.0, 15.0, 10.0),
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black12,
+                            offset: Offset(0, 2),
+                            blurRadius: 6.0,
+                          ),
+                        ],
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: BarChart(weeklySpending),
+                    );
+                  } else {
+                    final category = categories[i - 1];
+                    double totalAmountSpent = 0.0;
+
+                    category.expenses.forEach((expense) {
+                      totalAmountSpent += expense.cost;
+                    });
+
+                    return _buildCategory(category, totalAmountSpent);
+                  }
                 },
-                childCount: 1,
+                childCount: 1 + categories.length,
               ),
             ),
           ],
--- lib/src/widgets/bar_chart.dart
+++ lib/src/widgets/bar_chart.dart
@@ -14,7 +14,7 @@ class BarChart extends StatelessWidget {
     });
 
     return Padding(
-      padding: const EdgeInsets.all(10.0),
+      padding: const EdgeInsets.all(8.0),
       child: Column(
         children: [
           const Text(
